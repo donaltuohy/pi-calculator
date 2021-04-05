f6fc2dca3072fe19dec0180d1d5f85e1f6a2909d
Donal Tuohy <dtuohy@zendesk.com>
2021-04-05 17:20:05 +0100

    Server now distributing requests to available workers/

--- server/Server.py
+++ server/Server.py
@@ -1,30 +1,52 @@
 import config
+import logging
 from WorkerClient import WorkerClient
+
+logging.basicConfig(level=logging.INFO)
 
 class Server:
 
     def __init__(self):
-        print("Initialising server")
-        self.workers = self.get_available_workers()
-        print("Available workers: " + str(self.workers))
+        logging.info("Initialising server.")
+        print(self.calculate(20, 3))
 
+    def calculate(self, n, cycles):
+        available_workers = self.get_available_workers()
+        logging.info("Distributing work amongst " + str(len(available_workers)) + " workers")
+        result = self.distribute_work(self.split_up_work(n, cycles), self.get_available_workers())
+        
+        return result
 
-    def distribute_work(self, n, cycles):
-        print("Hello - got a server method")
-        return 0 
+    def split_up_work(self, n, cycles):
+        numbers_per_cycle = n / cycles
+        list = range(n)
+        res = []
+        for i in range(0, n, numbers_per_cycle):
+            res.append(list[i:i + numbers_per_cycle])
+        return res
+
+    # Using round robin to send the work to each available worker
+    def distribute_work(self, pieces_of_work, available_workers):
+        result = []
+        for i, work in enumerate(pieces_of_work, start=0):
+            worker_id = i % (len(available_workers))
+            worker = available_workers[worker_id]
+            worker_result = worker.calculate(work[0], work[len(work) - 1])
+            result.append(worker_result)
+        return result
 
     def get_available_workers(self):
-        available_workers = {}
+        available_workers = []
         
         for worker_config in config.workers:    
             worker = WorkerClient(worker_config)
            
             if(worker.ping()):
-                print("[ Worker - " + str(worker.get_base_url()) + "]" + ": PING successful, marking healthy")
+                logging.info("[Worker - " + str(worker.get_base_url()) + "]" + ": PING successful, marking healthy")
                 # Use the base url as the id for the worker - makes sure it's unique
-                available_workers[worker.get_base_url] = worker
+                available_workers.append(worker)
             else:
-                print("[ Worker - " + worker.get_base_url + "]" + ": PING unsuccessful, Marking unhealthy.")
+                logging.info("[Worker - " + worker.get_base_url() + "]" + ": PING unsuccessful, marking unhealthy.")
         
         return available_workers
 
--- server/WorkerClient.py
+++ server/WorkerClient.py
@@ -1,4 +1,7 @@
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 class WorkerClient:
 
@@ -7,18 +10,22 @@ class WorkerClient:
         self.port = config['port']
 
     def get_base_url(self):
-        return (self.address + ":" +  str(self.port))
+        base_url = self.address + ":" +  str(self.port)
+        return base_url
 
     def get_url(self, uri):
         return self.get_base_url() + uri
 
     def ping(self):
-        response = requests.get(self.get_url("/ping"))
-        print("\n")
-        print(self.get_base_url() + str(response.status))
-        print("\n")
-        return True
+        try:
+            response = requests.get(self.get_url("/ping"))
+            return response.status_code == 200
+        except Exception as exception:
+            logging.error("[Worker - " + self.get_base_url() + "] Error pinging worker: " + str(exception) )
+            return False
 
     def calculate(self, start, end):
-        response = requests.post(self.getUrl("/calculate"), json=shaDict)
-        return response
+        logging.info("[Worker - " + self.get_base_url() + "] Sending request to calculate from " + str(start) + " to " + str(end))
+        # body = "" #build json here to send to worker
+        # response = requests.post(self.getUrl("/calculate"))
+        return 0
